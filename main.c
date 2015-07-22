deecc5e5632e92310b89d61bcf38585485b6474f
Ryan Reyes <ryan.david.reyes@gmail.com>
2015-07-22 14:10:39 -0400

    Added comments and can display parent column.
    Commented the majority of the code, and the code can now display parent column.

--- main.c
+++ main.c
@@ -13,10 +13,16 @@
 #define DEFAULT_CONFIG_FILE ".rdirrc"
 #define ENTER 0x10
 
+/**********************************/
+/*        ENUM DEFINITIONS        */
+/**********************************/
 typedef enum {RUNNING, EXITING} program_states_t; //overall state of program
 //movement of folder up the tree or down the tree
 typedef enum {STAY, UP, DOWN, NEXT, PREV} dir_movement_t;
 
+/**********************************/
+/*       STRUCT DEFINITIONS       */
+/**********************************/
 typedef struct {
   size_t max_rows;
   size_t max_columns;
@@ -40,74 +46,106 @@ typedef struct {
   directory_entry_t * entries;
 } directory_entry_list_t;
 
-void init_terminal(screen_t *screen);
+
+/**********************************/
+/*       FUNCTION PROTOTYPES      */
+/**********************************/
+void initTerminal(screen_t *screen);
+void initSettings();
 int readConfig(char * config_file_name);
 void handleKeys(char input, dir_command_t *dir_command);
 int listDirs(directory_entry_list_t *dir_list, char * path);
 void clearDirList(directory_entry_list_t *dir_list);
+void signal_handler(int param);
 
+
+/**********************************/
+/*          GLOBAL STATE          */
+/**********************************/
+static struct Rdir {
+  program_states_t state;
+  size_t parent_column_width;
+  size_t current_column_width;
+  size_t child_column_width;
+} rdir;
+
+/**********************************/
+/*             MAIN               */
+/**********************************/
 int main(int argc, char ** argv) {
   screen_t screen;
-  program_states_t state = RUNNING;
   //dir_command_t dir_command;
   char * config_file_name = DEFAULT_CONFIG_FILE;
 
-  //directory_entry_list_t dir_parent;    //directory entry list for parent dir
+  directory_entry_list_t dir_parent;    //directory entry list for parent dir
   //directory_entry_list_t dir_child;     //directory entry list for highlighted dir
   directory_entry_list_t dir_current;   //direntry list for current working dir
 
   char input;
 
   //ncurses initialization
-  init_terminal(&screen);
+  initTerminal(&screen);
 
+  initSettings();
   //read configuration file
   if (readConfig(config_file_name) == -1) {
-    endwin();
-    fprintf(stderr, "Config file error");
+    fprintf(stderr, "using defaults");
   }
 
   //main loop
-  while (state == RUNNING) {
+  while (rdir.state == RUNNING) {
     //get directories
     listDirs(&dir_current, ".");
-    //listDirs(&dir_parent, "..");
+    listDirs(&dir_parent, "..");
     //listDirs(&dir_child, ".");
     //draw screen
+    for(int i = 0; i < dir_parent.capacity; ++i) {
+      mvprintw(i, 0, "%s", dir_parent.entries[i].basename);
+    }
     for(int i = 0; i < dir_current.capacity; ++i) {
-      mvprintw(i, 0, "%s", dir_current.entries[i].basename);
+      mvprintw(i, rdir.parent_column_width, "%s", dir_current.entries[i].basename);
     }
 
     input = getch();
     if (input == 'q')
-      state = EXITING;
+      rdir.state = EXITING;
     //handleKeys(input, &dir_command); //handle keys and exec key action
 
     clearDirList(&dir_current);
+    clearDirList(&dir_parent);
   } //end while (state)
 
   clearDirList(&dir_current);
+  clearDirList(&dir_parent);
   endwin();
   return 0;
 } //end main
 
 /*
- * init_terminal perform all initialization regarding the terminal and ncurses
+ * initSettings initializes the values in the global struct
  */
-void init_terminal(screen_t *screen) {
+void initSettings() {
+  rdir.state = RUNNING;
+  rdir.parent_column_width = 30;
+  rdir.child_column_width = 30;
+  rdir.current_column_width = 30;
+}
+/*
+ * initTerminal perform all initialization regarding the terminal and ncurses
+ */
+void initTerminal(screen_t *screen) {
   initscr();  //start curses mode
   getmaxyx(stdscr, screen->max_rows, screen->max_columns);
   raw();      //disable line buffering (no need to press enter with getch());
   keypad(stdscr, TRUE); //allows the user to read F1..arrow keys
   noecho();             //no echo with getch
-} //end void init_terminal
+} //end void initTerminal
 
 /*
  * Read config reads the configuration file (.rdirrc by default)
  * and sets the corresponding structs.
  */
 int readConfig(char * config_file_name) {
-
   FILE * file_p;
 
   file_p = fopen(config_file_name, "r");
@@ -171,6 +209,9 @@ int listDirs(directory_entry_list_t *dir_list, char * path) {
   return 0;
 } //end listDirs
 
+/*
+ * clearDirList deletes the directory lists with the folders in path
+ */
 void clearDirList(directory_entry_list_t *dir_list) {
   if (dir_list->entries != NULL) {
     //clear all names
@@ -187,3 +228,10 @@ void clearDirList(directory_entry_list_t *dir_list) {
     dir_list->capacity = 0;
   }
 } //end clearDirList
+
+/*
+ * Signal handler sets state var to exit
+ */
+void signal_handler(int param) {
+  rdir.state = EXITING;
+}
