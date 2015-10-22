22279897f652baba67bdf549d8d6d8e156b7d21b
Ryan Reyes <ryan.david.reyes@gmail.com>
2015-10-22 19:53:42 -0400

    Added shell function to auto cd to selected dir.
    rcd takes the directory/file returned from rdir, and then cd's to the
    directory or the directory containing the file.

--- rdir.sh
+++ rdir.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
 
 function rdir() {
-  ./rdir.x 2> /tmp/rdir
+  rdir.x 2> /tmp/rdir
   ERROR=$(</tmp/rdir)
   rm /tmp/rdir
   echo $ERROR | sed 's/using defaults//'
+}
+
+function rcd() {
+  rdir.x 2> /tmp/rdir
+  ERROR=$(</tmp/rdir)
+  rm /tmp/rdir
+
+  echo $ERROR | sed 's/using defaults//'
+  DIR=$(echo $ERROR | sed 's/using defaults//')
+
+  if [[ -d $DIR ]]; then
+    builtin cd $DIR
+  elif [[ -f $DIR ]]; then
+    builtin cd "$(dirname $DIR)"
+  fi
 }
