47f3ddc982b7fad50c9196824635003102b1e997
Ryan Reyes <ryan.david.reyes@gmail.com>
2016-05-24 02:27:52 -0400

    Added zsh integration!
    C-e to open up rdir and change directories.
    C-f to use rdir to select a file and append it to the current line.

--- rdir.sh
+++ rdir.sh
@@ -21,3 +21,34 @@ function c() {
     builtin cd "$(dirname $DIR)"
   fi
 }
+
+
+function _rdir_explore() {
+  rdir.x 2> /tmp/rdir <$TTY
+  ERROR=$(</tmp/rdir)
+  rm /tmp/rdir
+
+  echo $ERROR
+  DIR=$(echo $ERROR)
+
+  if [[ -d $DIR ]]; then
+    builtin cd $DIR
+  elif [[ -f $DIR ]]; then
+    builtin cd "$(dirname $DIR)"
+  fi
+  zle reset-prompt
+}
+
+function _rdir_sel_widget() {
+  rdir.x 2> /tmp/rdir <$TTY
+  ERROR=$(</tmp/rdir)
+  rm /tmp/rdir
+
+  LBUFFER="${LBUFFER}${ERROR}"
+  zle redisplay
+}
+
+zle -N _rdir_explore
+zle -N _rdir_sel_widget
+bindkey '^E' _rdir_explore
+bindkey '^F' _rdir_sel_widget
